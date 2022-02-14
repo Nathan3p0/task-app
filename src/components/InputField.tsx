6d4869a128b0d2e2c65bae31eaf94850273a6694
Nate Szelag <nszelag@getmatterhorn.com>
2022-02-14 00:25:30 -0500

    create logic to add todos

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import InputFieldProps from "../interfaces/InputFieldProps";
 import "./styles.css";
 
-const InputField = ({ todo, setTodo }: InputFieldProps) => {
+const InputField = ({ todo, setTodo, handleAdd }: InputFieldProps) => {
 	return (
-		<form className="input">
+		<form className="input" onSubmit={handleAdd}>
 			<input type="text" placeholder="Enter a task" className="input__box" value={todo} onChange={(e) => setTodo(e.target.value)} />
 			<button className="input__submit" type="submit">
 				Go
--- src/interfaces/InputFieldProps.ts
+++ src/interfaces/InputFieldProps.ts
@@ -3,6 +3,7 @@ import React from "react";
 interface InputFieldProps {
 	todo: string;
 	setTodo: React.Dispatch<React.SetStateAction<string>>;
+	handleAdd: (e: React.FormEvent) => void;
 }
 
 export default InputFieldProps;
