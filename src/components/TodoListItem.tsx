010d35d30f0738f7b2334f652be99a3427ccfc21
Nate Szelag <nszelag@getmatterhorn.com>
2022-02-14 01:06:31 -0500

    add functionality to complete and delete todos

--- src/components/TodoListItem.tsx
+++ src/components/TodoListItem.tsx
@@ -5,17 +5,25 @@ import { AiFillEdit, AiFillDelete } from "react-icons/ai";
 import { MdDone } from "react-icons/md";
 
 export const TodoListItem = ({ todo, todos, setTodos }: TodoListItemProps) => {
+	const handleDone = (id: number) => {
+		setTodos(todos.map((todo) => (todo.id === id ? { ...todo, isDone: !todo.isDone } : todo)));
+	};
+
+	const handleDelete = (id: number) => {
+		setTodos(todos.filter((todo) => todo.id !== id));
+	};
+
 	return (
 		<form className="todos__single">
-			<span className="todos__single--text">{todo.todo}</span>
+			{todo.isDone ? <s className="todos__single--text">{todo.todo}</s> : <span className="todos__single--text">{todo.todo}</span>}
 			<div>
 				<span className="icon">
 					<AiFillEdit />
 				</span>
-				<span className="icon">
+				<span className="icon" onClick={() => handleDelete(todo.id)}>
 					<AiFillDelete />
 				</span>
-				<span className="icon">
+				<span className="icon" onClick={() => handleDone(todo.id)}>
 					<MdDone />
 				</span>
 			</div>
