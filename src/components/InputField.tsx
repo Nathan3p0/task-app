4412275c87b7c159f3a34ed98d041aea20785015
Nate Szelag <nszelag@getmatterhorn.com>
2022-02-14 00:30:37 -0500

    add useRef to onSubmit handler to remove input field focus when submitted

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useRef } from "react";
 import InputFieldProps from "../interfaces/InputFieldProps";
 import "./styles.css";
 
 const InputField = ({ todo, setTodo, handleAdd }: InputFieldProps) => {
+	const inputRef = useRef<HTMLInputElement>(null);
 	return (
-		<form className="input" onSubmit={handleAdd}>
-			<input type="text" placeholder="Enter a task" className="input__box" value={todo} onChange={(e) => setTodo(e.target.value)} />
+		<form
+			className="input"
+			onSubmit={(e) => {
+				handleAdd(e);
+				inputRef.current?.blur();
+			}}
+		>
+			<input ref={inputRef} type="text" placeholder="Enter a task" className="input__box" value={todo} onChange={(e) => setTodo(e.target.value)} />
 			<button className="input__submit" type="submit">
 				Go
 			</button>
