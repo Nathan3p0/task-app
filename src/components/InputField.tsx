958bedd5f773aee67ee8aac35bd042812f1eb82c
Nate Szelag <nszelag@getmatterhorn.com>
2022-02-14 00:15:30 -0500

    pass props to input field component and create props interface

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import InputFieldProps from "../interfaces/InputFieldProps";
 import "./styles.css";
 
-const InputField: React.FC = () => {
+const InputField = ({ todo, setTodo }: InputFieldProps) => {
 	return (
 		<form className="input">
-			<input type="text" placeholder="Enter a task" className="input__box" />
+			<input type="text" placeholder="Enter a task" className="input__box" value={todo} onChange={(e) => setTodo(e.target.value)} />
 			<button className="input__submit" type="submit">
 				Go
 			</button>
---
+++ src/interfaces/InputFieldProps.ts
@@ -0,0 +1,8 @@
+import React from "react";
+
+interface InputFieldProps {
+	todo: string;
+	setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export default InputFieldProps;
