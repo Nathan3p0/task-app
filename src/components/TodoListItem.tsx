6e4253365e70ecbaa91fbc227c12a6cf4bfde0c0
Nate Szelag <nszelag@getmatterhorn.com>
2022-02-14 00:58:02 -0500

    add icons and styling to todo component

--- src/components/TodoListItem.tsx
+++ src/components/TodoListItem.tsx
@@ -1,7 +1,24 @@
 import React from "react";
 import Todo from "../interfaces/Todo";
 import { TodoListItemProps } from "../interfaces/TodoListItemProps";
+import { AiFillEdit, AiFillDelete } from "react-icons/ai";
+import { MdDone } from "react-icons/md";
 
 export const TodoListItem = ({ todo, todos, setTodos }: TodoListItemProps) => {
-	return <div>TodoListItem</div>;
+	return (
+		<form className="todos__single">
+			<span className="todos__single--text">{todo.todo}</span>
+			<div>
+				<span className="icon">
+					<AiFillEdit />
+				</span>
+				<span className="icon">
+					<AiFillDelete />
+				</span>
+				<span className="icon">
+					<MdDone />
+				</span>
+			</div>
+		</form>
+	);
 };
